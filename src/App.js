21ee39f6213fe1376bcd230859a8e5e649e27c47
skyv26 <dev.aakashv@gmail.com>
2022-10-14 15:25:20 +0530

    Refactor `[App.js]` file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router';
+import Home from './pages/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/rockets" element={<Home />} />
+      <Route path="/" element={<Navigate replace to="/rockets" />} />
+    </Routes>
+  </>
+);
 
 export default App;
