d4774605008997d5c2919e5ef5321dd333a7693a
skyv26 <dev.aakashv@gmail.com>
2022-10-14 15:54:14 +0530

    Add `globalStyle` file

---
+++ src/globalStyles.styled.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
+  
+  html {
+    font-size: 62.5%;
+    height: 100%;
+  }
+
+  body {
+    width: 100%;
+    min-height: 100%;
+  }
+`;
+
+export default GlobalStyle;
