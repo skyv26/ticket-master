1fce72b64b790f944546974d12c30309f5b9a6c4
skyv26 <dev.aakashv@gmail.com>
2022-10-14 16:04:32 +0530

    Add `Header` component to `[App.js]`

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router';
 import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
 
 const App = () => (
   <>
+    <Header />
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/" element={<Navigate replace to="/home" />} />
