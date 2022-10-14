7ba8bf1598a73f2fe505f97c34cb910036bc3eb2
skyv26 <dev.aakashv@gmail.com>
2022-10-14 15:26:47 +0530

    Change the `path` for home page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from './pages/Home/Home';
 const App = () => (
   <>
     <Routes>
-      <Route path="/rockets" element={<Home />} />
-      <Route path="/" element={<Navigate replace to="/rockets" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigate replace to="/home" />} />
     </Routes>
   </>
 );
