3605f0b140f58cd27e5f0940871285fe9b73fad1
skyv26 <dev.aakashv@gmail.com>
2022-10-15 19:25:12 +0530

    Resolve linters error

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CategoryPage = props => {
+const CategoryPage = (props) => {
   const { img } = props;
   return (
     <div>
