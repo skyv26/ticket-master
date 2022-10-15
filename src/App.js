0a6855e9cf2e0d524750811390ac0cc45afad00d
skyv26 <dev.aakashv@gmail.com>
2022-10-15 22:34:49 +0530

    Resolve the `route` issues

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
+import CategoryPage from './pages/CategoryPage/CategoryPage';
 import { TicketThunk } from './redux/ticketSlice';
 
 const App = () => {
@@ -10,12 +11,12 @@ const App = () => {
   useEffect(() => {
     dispatch(TicketThunk());
   }, [dispatch]);
-
   return (
     <>
       <Header />
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/category" element={<CategoryPage />} />
         <Route path="/" element={<Navigate replace to="/home" />} />
       </Routes>
     </>
