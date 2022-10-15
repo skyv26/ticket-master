e3d991c7acda9561504e4dc4ea3e81f037b46bd6
skyv26 <dev.aakashv@gmail.com>
2022-10-15 16:44:12 +0530

    Update the `slice` file

--- src/redux/ticketSlice.js
+++ src/redux/ticketSlice.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const TicketAPI = 'https://app.ticketmaster.com/discovery/v2/events.json?&apikey=VwRqkplVGaYm4ZuUaAV3sa4LSGGl5eYa&size=100';
+const TicketAPI = 'https://app.ticketmaster.com/discovery/v2/events.json?&apikey=VwRqkplVGaYm4ZuUaAV3sa4LSGGl5eYa&size=25';
 const TicketType = '/get/ticket';
 
 const initialState = {};
 
-const COROUSEL_LIMIT = 3;
+const COROUSEL_LIMIT = 4;
+
+// const CATEGORIES = {};
 
 const COROUSEL_ARRAY = [];
 
@@ -16,8 +19,13 @@ for (let iter = 0; iter < COROUSEL_LIMIT; iter += 1) {
 }
 
 export const TicketThunk = createAsyncThunk(TicketType, async () => {
-  const data = await (await fetch(TicketAPI)).json();
-  return data;
+  const sport = await (await fetch(`${TicketAPI}&classificationName=sports`)).json();
+  const music = await (await fetch(`${TicketAPI}&classificationName=music`)).json();
+  const art = await (await fetch(`${TicketAPI}&classificationName=art`)).json();
+  const misc = await (await fetch(`${TicketAPI}&classificationName=miscellaneous`)).json();
+  return {
+    sport, music, art, misc,
+  };
 });
 
 const TicketSlice = createSlice({
@@ -27,15 +35,31 @@ const TicketSlice = createSlice({
   extraReducers: {
     [TicketThunk.fulfilled]: (state, action) => {
       const { payload } = action;
-      // eslint-disable-next-line no-underscore-dangle
-      const { events } = payload._embedded;
-      // eslint-disable-next-line no-underscore-dangle
-      const links = payload._links;
+      const newState = {
+        page: {
+          size: payload.music.page.size
+          + payload.art.page.size
+          + payload.misc.page.size
+          + payload.sport.page.size,
+          totalElements: payload.music.page.totalElements
+          + payload.art.page.totalElements
+          + payload.misc.page.totalElements
+          + payload.sport.page.totalElements,
+          totalPages: payload.music.page.totalPages
+          + payload.art.page.totalPages
+          + payload.misc.page.totalPages
+          + payload.sport.page.totalPages,
+        },
+        events: [
+          ...payload.music._embedded.events,
+          ...payload.art._embedded.events,
+          ...payload.misc._embedded.events,
+          ...payload.sport._embedded.events,
+        ],
+      };
+      const corousel = COROUSEL_ARRAY.map((each) => newState.events[each]);
       return {
-        page: payload.page,
-        events,
-        links,
-        corousel: COROUSEL_ARRAY.map((each) => events[each]),
+        newState, corousel,
       };
     },
   },
