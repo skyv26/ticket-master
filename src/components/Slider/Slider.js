1a7569d0d58c90fef902bb463967b1392f286a80
skyv26 <dev.aakashv@gmail.com>
2022-10-15 19:23:22 +0530

    Add `[Slider.js]` component

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Image, ImageOverlay } from '../../globalStyles.styled';
 import {
   SliderWrapper,
@@ -7,18 +8,18 @@ import {
   ImageOverlayContainer,
 } from './Slider.styled';
 
-const imgArray = ['https://www.gstatic.com/webp/gallery/1.webp', 'https://www.gstatic.com/webp/gallery/2.webp', 'https://www.gstatic.com/webp/gallery/4.webp'];
+// const imgArray = ['https://www.gstatic.com/webp/gallery/1.webp', 'https://www.gstatic.com/webp/gallery/2.webp', 'https://www.gstatic.com/webp/gallery/4.webp'];
 
 const Slider = () => {
+  const tickets = useSelector((state) => state.tickets);
   const [cSrc, uSrc] = useState(0);
-
   useEffect(() => {
     const sliders = document.querySelectorAll(SliderList);
     const timer = setInterval(() => uSrc((prev) => {
       sliders.forEach((each) => {
         each.classList.remove('active');
       });
-      if (prev < 2) {
+      if (prev < 3) {
         sliders[prev + 1].classList.add('active');
         return prev + 1;
       }
@@ -27,27 +28,19 @@ const Slider = () => {
     }), 4000);
     return () => clearTimeout(timer);
   }, []);
-
-  const sliderDot = [
-    {
-      id: 1,
-    },
-    {
-      id: 2,
-    },
-    {
-      id: 3,
-    },
-  ];
+  if (Object.keys(tickets).length === 0) {
+    return <></>;
+  }
+  const { corousel } = tickets;
 
   return (
     <SliderWrapper>
       <ImageOverlayContainer>
-        <Image src={imgArray[cSrc]} alt="game 1" />
+        <Image src={corousel[cSrc].images[2].url} alt="game 1" />
         <ImageOverlay />
       </ImageOverlayContainer>
       <SliderListContainer>
-        {sliderDot.map((each) => <SliderList key={each.id} className={`slider-${each.id} ${each.id === 1 ? 'active' : ''}`} />)}
+        {corousel.map((each, indx) => <SliderList key={each.id} className={`slider-${each.id} ${indx === 0 ? 'active' : ''}`} />)}
       </SliderListContainer>
     </SliderWrapper>
   );
