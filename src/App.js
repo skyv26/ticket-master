277f5b46c775eac9013bb91a971b976292ee0dfe
skyv26 <dev.aakashv@gmail.com>
2022-10-15 22:56:41 +0530

    Add the route for `[Details]` page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 import Home from './pages/Home/Home';
+import Details from './pages/Details/Details';
 import Header from './components/Header/Header';
 import CategoryPage from './pages/CategoryPage/CategoryPage';
 import { TicketThunk } from './redux/ticketSlice';
@@ -17,6 +18,7 @@ const App = () => {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/category" element={<CategoryPage />} />
+        <Route path="/details" element={<Details />} />
         <Route path="/" element={<Navigate replace to="/home" />} />
       </Routes>
     </>
