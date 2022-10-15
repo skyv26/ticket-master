f91e6f659539e01df2eb80be2a10c96ddd0dc2f4
skyv26 <dev.aakashv@gmail.com>
2022-10-15 22:35:44 +0530

    Implement the back button functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import { Logo } from '../../globalStyles.styled';
 import {
   AppHeader,
@@ -8,19 +9,31 @@ import {
   HeaderWrapper,
 } from './Header.styled';
 
-const Header = () => (
-  <>
-    <AppHeader>
-      <HeaderWrapper>
-        <BackButton />
-        <Logo>MyTicketO</Logo>
-      </HeaderWrapper>
-      <HeaderWrapper>
-        <Microphone />
-        <MenuBurger />
-      </HeaderWrapper>
-    </AppHeader>
-  </>
-);
+const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const historyBtnHandle = () => {
+    const { pathname } = location;
+    if (pathname.includes('category')) {
+      navigate('/');
+    }
+  };
+
+  return (
+    <>
+      <AppHeader>
+        <HeaderWrapper>
+          <BackButton onClick={historyBtnHandle} />
+          <Logo>MyTicketO</Logo>
+        </HeaderWrapper>
+        <HeaderWrapper>
+          <Microphone />
+          <MenuBurger />
+        </HeaderWrapper>
+      </AppHeader>
+    </>
+  );
+};
 
 export default Header;
